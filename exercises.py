d8d5f58adf27b943066f8c889ccaa4a1f651b8a7
Tobias Adolph <tobias.adolph@lrz.de>
2016-10-19 17:26:21 +0200

    Token handling revised (fixes #25)
    From now on the token must not be given in the config-file,
    but either by param (--token) or in cwd as file '.TOKEN'

--- exercises.py
+++ exercises.py
@@ -8,7 +8,7 @@ from pprint import pprint
 
 def check_config(config):
     configOk = True
-    for key in [ "url", "token", "pattern" ]:
+    for key in [ "url", "pattern" ]:
         if not key in config:
             print "Config-key %s is not set!" % key
             configOk = False
@@ -137,6 +137,9 @@ def main():
                             required = False,
                             help = """Comma separated list of mail addresses of reviewers 
                             for the exercise. Only used with action add_reviewer (or publish?).""")
+    parser.add_argument(    '--token',
+                            required = False,
+                            help = """gitlab Auth-Token""")
 
     args = parser.parse_args()
     try:
@@ -146,6 +149,17 @@ def main():
     except IOError as e:
         print "Couldn't open {0}: I/O error({1}) - {2}".format(args.config_file, e.errno, e.strerror)
         sys.exit(1) 
+    if not args.token or args.token == None:
+        try:
+            with open(".TOKEN") as token_file:
+                config["token"] = token_file.readline().rstrip()
+            token_file.close()
+        except IOError as e:
+            print "No token file in cwd and no token given via --token param. Aborting!"
+            sys.exit(4)
+    else:
+        config["token"] = args.token
+
     if not check_config(config):
         print "Config check failed"
         sys.exit(2)
