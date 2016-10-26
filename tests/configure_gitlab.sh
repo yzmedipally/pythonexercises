fa614a43bf34aa48c7ca4442a0dd2095f994806e
Tobias Adolph <tobias.adolph@lrz.de>
2016-10-26 16:10:00 +0200

    ci-debugging (39)

--- tests/configure_gitlab.sh
+++ tests/configure_gitlab.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+success=1
+while [ "$success" != "0" ]
+do
+	nc -z -v localhost 80
+	success=$?
+	if [ "$success" == "0" ]
+	then
+		echo "gitlab's up and fine!"
+	else	
+		echo "gitlab's not up yet: $success"
+		sleep 5
+	fi
+done
+
 gitlab-rails console production <<GITLAB_COMMAND_LINE
 user = User.where(id: 1).first
 user.password = 'test1234'
