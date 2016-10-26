7d9e7015cb82b2fcffd2ba126fdc1acb472c493b
Tobias Adolph <tobias.adolph@lrz.de>
2016-10-26 16:00:16 +0200

    ci-debugging (38)

--- tests/configure_gitlab.sh
+++ tests/configure_gitlab.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-curl -I localhost:80
+gitlab-rails console production <<GITLAB_COMMAND_LINE
+user = User.where(id: 1).first
+user.password = 'test1234'
+user.password_confirmation = 'test1234'
+user.password_automatically_set = false
+user.save!
+exit
+GITLAB_COMMAND_LINE
+
+curl -I localhost/users/sign_in
+
